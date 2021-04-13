dd76a933c5b09e94dab05b07d27428bce29f8cbc
Bram De Kneef <bram@dekneef.be>
2021-04-13 18:39:06 +0200

    hamburgermenu gefixt + witte balken aan de zijkant

--- javascript/script.js
+++ javascript/script.js
@@ -4,4 +4,7 @@ const navbar = document.querySelector(".topnav");
 
 menuIcon.addEventListener('click', () => {
   navbar.classList.toggle('change');
+  menuIcon.classList.toggle('cross');
+  
+
 });
