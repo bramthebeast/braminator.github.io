1275900b8b18c58e69586116515cd7772bca13e0
Bram De Kneef <bram@dekneef.be>
2021-04-13 19:58:00 +0200

    menu gefixt

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,13 @@
 const menuIcon = document.querySelector(".icon");
 
+const body = document.querySelector(".body")
+
 const navbar = document.querySelector(".topnav");
 
 menuIcon.addEventListener('click', () => {
   navbar.classList.toggle('change');
   menuIcon.classList.toggle('cross');
-  
+  body.classList.toggle('scroll')
+
 
 });
